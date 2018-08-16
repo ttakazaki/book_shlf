c9f5bbe96c1b245a915ec503f959c99225b9f8a1
Toshifumi Takazaki <urd59675@gmail.com>
2018-08-16 19:12:49 +0900

    first commit

---
+++ server/config/config.js
@@ -0,0 +1,14 @@
+const config = {
+  production: {
+    SECRET: process.env.SECRET,
+    DATABASE: process.env.MONGO_URI
+  },
+  default: {
+    SECRET: 'SUPERSECRETPASSWORD123',
+    DATABASE: 'mongodb://user:user@localhost:27017/sample'
+  }
+};
+
+exports.get = function get(env) {
+  return config[env] || config.default;
+};
